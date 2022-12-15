f4321ca507765324421ffea166dbde23bad91d74
blushedmw <120435118+blushedmw@users.noreply.github.com>
2022-12-15 17:08:22 +0900

    Update 데이터저널리즘_4조.py

--- 데이터저널리즘_4조.py
+++ 데이터저널리즘_4조.py
@@ -68,13 +68,7 @@ with col1:
     st.markdown('* 미성년자&성인 교제')
     q1 = pd.DataFrame({'labels':['positive', 'nuetral', 'negative'], 'n_ratio':[3/102*100,9/102*100,90/102*100]})
     q1fig = px.pie(q1, values='n_ratio', names='labels', width=300)
-    q1fig.update_layout(legend=dict(
-    orientation="h",
-    yanchor="bottom",
-    y=1.02,
-    xanchor="right",
-    x=1
-))
+    q1fig.update_layout(showlegend=False)
     st.plotly_chart(q1fig)
 with col2:
     st.markdown('* 미성년자&성인 교제')
