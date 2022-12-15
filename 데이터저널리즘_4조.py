d7728537971f0016dd467d14be2150dfc0794076
blushedmw <120435118+blushedmw@users.noreply.github.com>
2022-12-15 17:10:22 +0900

    Update 데이터저널리즘_4조.py

--- 데이터저널리즘_4조.py
+++ 데이터저널리즘_4조.py
@@ -63,26 +63,28 @@ st.success('''
 
 # plt.rcParams['font.family'] = 'NanumSquare'
 col1, col2, col3 = st.columns(3)
-explode = (0, 0, 0) 
 with col1:
     st.markdown('* 미성년자&성인 교제')
-    q1 = pd.DataFrame({'labels':['positive', 'nuetral', 'negative'], 'n_ratio':[3/102*100,9/102*100,90/102*100]})
-    q1fig = px.pie(q1, values='n_ratio', names='labels', width=300)
-    q1fig.update_layout(showlegend=False)
-    st.plotly_chart(q1fig)
+    labels = 'positive', 'nuetral', 'negative'
+    sizes = [3/102*100,9/102*100,90/102*100]
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=False, startangle=90, textprops={'fontsize': 16}, labeldistance=1.5, pctdistance=1.2)
+    ax1.axis('equal') 
+    st.pyplot(fig1)
 with col2:
-    st.markdown('* 미성년자&성인 교제')
-    q2 = pd.DataFrame({'labels':['positive', 'nuetral', 'negative'], 'n_ratio':[1/102*100,6/102*100,95/102*100]})
-    q2fig = px.pie(q2, values='n_ratio', names='labels')
-    st.plotly_chart(q2fig)
-#     st.markdown('* 미성년자&성인 임신, 출산')
-#     labels = 'p', 'nu', 'negative'
-#     sizes = [1/102*100,6/102*100,95/102*100]
-#     fig2, ax2 = plt.subplots()
-#     ax2.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-#             shadow=False, startangle=90, textprops={'fontsize': 16}, labeldistance=1.5, pctdistance=1.2)
-#     ax2.axis('equal')
-#     st.pyplot(fig2)
+#     st.markdown('* 미성년자&성인 교제')
+#     q2 = pd.DataFrame({'labels':['positive', 'nuetral', 'negative'], 'n_ratio':[1/102*100,6/102*100,95/102*100]})
+#     q2fig = px.pie(q2, values='n_ratio', names='labels')
+#     st.plotly_chart(q2fig)
+    st.markdown('* 미성년자&성인 임신, 출산')
+    labels = 'p', 'nu', 'negative'
+    sizes = [1/102*100,6/102*100,95/102*100]
+    fig2, ax2 = plt.subplots()
+    ax2.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=False, startangle=90, textprops={'fontsize': 16}, labeldistance=1.5, pctdistance=1.2)
+    ax2.axis('equal')
+    st.pyplot(fig2)
 with col3:
     st.markdown('* 미성년자 간 임신, 출산')
     labels = 'p', 'nu', 'negative'
